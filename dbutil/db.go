5948466468bcb06853ecee3db6f75595773441a8
alanrong <alanrong@gmail.com>
2017-12-04 15:53:42 +0800

    add dbutil

---
+++ dbutil/db.go
@@ -0,0 +1,16 @@
+package dbutil
+
+import "database/sql"
+
+//NewDB return db connection
+func NewDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
