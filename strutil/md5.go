839ec444321af3beac967315338e4055b4466a43
alanrong <alanrong@gmail.com>
2017-12-25 17:36:48 +0800

    add HMAC-SHA1

--- strutil/md5.go
+++ strutil/md5.go
@@ -1,7 +1,10 @@
 package strutil
 
 import (
+	"crypto/hmac"
+	"crypto/md5"
 	"crypto/sha1"
+	"encoding/base64"
 	"encoding/hex"
 )
 
@@ -9,4 +12,18 @@ import (
 func Sha1(content string) string {
 	hash := sha1.Sum([]byte(content))
 	return hex.EncodeToString(hash[:])
+}
+
+//MD5 gen hex md5 of content
+func MD5(content string) string {
+	hash := md5.Sum([]byte(content))
+	return hex.EncodeToString(hash[:])
+}
+
+//HmacSha1 gen HMAC-SHA1 of content with key
+func HmacSha1(content, key string) string {
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(content))
+	hash := mac.Sum(nil)
+	return base64.StdEncoding.EncodeToString(hash[:])
 }
--- strutil/str.go
+++ strutil/str.go
@@ -11,3 +11,9 @@ func GenSalt() string {
 	u := uuid.NewV4()
 	return strings.Join(strings.Split(u.String(), "-"), "")
 }
+
+//GenUUID generate 36byte uuid
+func GenUUID() string {
+	u := uuid.NewV4()
+	return u.String()
+}
